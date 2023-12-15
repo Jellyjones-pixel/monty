aca0fdd81b8a67d2ff9bcf5fe622db2fa0713f0c
Mburu Rionge <victormburu3@gmail.com>
2023-12-15 12:39:08 -0800

    prints elements of the stack

--- pall.c
+++ pall.c
@@ -1,10 +1,13 @@
 #include "monty.h"
+
 /**
  * f_pall - prints stack
- * @head: head of stack
+ * @header: head of stack
  * @counter: not used
+ * Description: This function prints the elements of the stack
  * Return: no return
 */
+
 void f_pall(stack_t **header, unsigned int counter)
 {
 	stack_t *h;
